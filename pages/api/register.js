1502f9c2dd63f3f7ce468a7b22891187d97c8835
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-12-31 14:11:33 -0500

    not opening registration for nextjs testing

--- pages/api/register.js
+++ pages/api/register.js
@@ -6,14 +6,15 @@ const handler = async (req, res) => {
   try {
     const hashed_password = await bcrypt.hash(req.body.password, 10);
 
-    await User.create({
-      profile_pic: 'https://field-observer.s3.ca-central-1.amazonaws.com/default-profile-pic.jpg',
-      username: req.body.username,
-      email: req.body.email,
-      password: hashed_password,
-    });
+    // await User.create({
+    //   profile_pic: 'https://field-observer.s3.ca-central-1.amazonaws.com/default-profile-pic.jpg',
+    //   username: req.body.username,
+    //   email: req.body.email,
+    //   password: hashed_password,
+    // });
 
-    res.json({ status: "ok", message: "Successfully registered user." });
+    // res.json({ status: "ok", message: "Successfully registered user." });
+    res.json({ status: "ok", message: "Not open for sign ups yet." });
   } catch (err) {
     return res.json({ status: "error", error: err });
   }
