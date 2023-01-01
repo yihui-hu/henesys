cb6f698eaa1abab7685ff8117936a1d4ba93f97a
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-01 12:11:22 -0500

    rollback

--- pages/api/upload-url-preview.js
+++ pages/api/upload-url-preview.js
@@ -9,8 +9,7 @@ const handler = async (req, res) => {
 
     try {
       browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        ignoreDefaultArgs: ['--disable-extensions'],
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
         executablePath: await chromium.executablePath,
         ignoreHTTPSErrors: true,
       });
@@ -30,7 +29,7 @@ const handler = async (req, res) => {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
     );
-    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
 
     try {
       const image_base64 = await page.screenshot({ encoding: "base64" });
