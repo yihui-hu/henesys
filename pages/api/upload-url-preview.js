c0fef484bd7c2ad0a4da7d7fed5fbe7e8b16195a
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-12-31 23:08:33 -0500

    deleted isreachable dependency

--- pages/api/upload-url-preview.js
+++ pages/api/upload-url-preview.js
@@ -1,52 +1,57 @@
-import chromium from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer-core'
-const reachableUrl = require("reachable-url");
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 const handler = async (req, res) => {
   try {
     let { url } = req.body;
 
-    if (reachableUrl.isReachable(await reachableUrl(url))) {
-      const browser = await puppeteer.launch({
-        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-      })
+    const executablePath =
+      (await chromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
 
-      const page = await browser.newPage();
-      await page.goto(url, { waitUntil: "domcontentloaded" });
+    const browser = await puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
 
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    );
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    try {
+      const image_base64 = await page.screenshot({ encoding: "base64" });
+
+      let title, description;
       try {
-        const image_base64 = await page.screenshot({ encoding: "base64" });
-
-        let title, description;
-        try {
-          title = await page.$eval(
-            "head > meta[property='og:title']",
-            (element) => element.content
-          );
-          description = await page.$eval(
-            "head > meta[property='og:description']",
-            (element) => element.content
-          );
-        } catch (err) {
-          console.log(err);
-        }
-
-        let metadata = {
-          title: title == undefined ? null : title,
-          description: description == undefined ? null : description,
-          image_base64: image_base64,
-        };
-
-        return res.json({ status: "ok", metadata: metadata });
+        title = await page.$eval(
+          "head > meta[property='og:title']",
+          (element) => element.content
+        );
+        description = await page.$eval(
+          "head > meta[property='og:description']",
+          (element) => element.content
+        );
       } catch (err) {
-        return res.json({ status: "error", error: "Error saving url." });
+        console.log(err);
       }
-    }
 
+      let metadata = {
+        title: title == undefined ? null : title,
+        description: description == undefined ? null : description,
+        image_base64: image_base64,
+      };
+
+      return res.json({ status: "ok", metadata: metadata });
+    } catch (err) {
+      return res.json({ status: "error", error: "Error saving url." });
+    }
   } catch (err) {
     return res.json({
       status: "error",
