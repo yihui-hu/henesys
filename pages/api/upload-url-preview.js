4340ddbdf714e529c229db5e7f25594c4322bec3
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-01 01:52:52 -0500

    dom content

--- pages/api/upload-url-preview.js
+++ pages/api/upload-url-preview.js
@@ -29,8 +29,7 @@ const handler = async (req, res) => {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
     );
-    await page.goto(url, { waitUntil: "domcontentloaded" });
-    await page.waitForTimeout(1000);
+    await page.goto(url, { waitUntil: "networkidle2" });
 
     try {
       const image_base64 = await page.screenshot({ encoding: "base64" });
