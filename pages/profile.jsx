6ee8cf7d910d1f1360cb1fb8488d5d3ee6a2af92
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-01 21:31:35 -0500

    done

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -11,14 +11,7 @@ export default function Profile({}) {
     const response = await fetch("/api/logout", {
       method: "POST",
     });
-
     const data = await response.json();
-
-    if (data.status == 200) {
-      alert("OK")
-    } else {
-      alert("NOPE")
-    }
 
     router.push("/");
   }
