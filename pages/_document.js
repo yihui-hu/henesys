d53dfc20c90ed1ff5dc51f0faa68dd4eddc4162f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-03 01:17:44 -0500

    centered add button + consolidated meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="field-observer" />
-        <meta name="apple-mobile-web-app-status-bar" content="#ffffff" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#fff" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
