eb398105cb02def025330647689997786176a442
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-08 16:58:15 -0500

    quality optimization (?)

--- components/FullFilePreview.jsx
+++ components/FullFilePreview.jsx
@@ -18,6 +18,7 @@ const FullFilePreview = ({ file, metadata }) => {
           <Image
             src={file}
             fill
+            quality={75}
             sizes="(max-width: 480px) 100vw,
                    (max-width: 1024px) 100vw,
                    70vw"
--- components/FullUrlPreview.jsx
+++ components/FullUrlPreview.jsx
@@ -11,6 +11,7 @@ const FullUrlPreview = ({ url, metadata }) => {
           <Image
             src={imageURL}
             fill
+            quality={75}
             sizes="(max-width: 480px) 100vw,
                    (max-width: 1024px) 100vw,
                    70vw"
