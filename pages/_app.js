bd5dcd7dda10c28a74d283b0d921949045823e47
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-09 18:08:09 -0500

    testing metatags

--- pages/_app.js
+++ pages/_app.js
@@ -25,11 +25,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* <title>henesys</title>
+        <title>henesys</title>
         <meta
           name="description"
           content="barebones bookmarking / note-taking site"
-        /> */}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
