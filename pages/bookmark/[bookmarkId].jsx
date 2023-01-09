5730350b520841289b642e78f69ae4a0673de88e
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-09 17:50:25 -0500

    og image not showing up (?)

--- pages/bookmark/[bookmarkId].jsx
+++ pages/bookmark/[bookmarkId].jsx
@@ -90,6 +90,7 @@ export default function BookmarkPage({ isLoggedIn }) {
             <meta property="og:site_name" content="henesys"/>
             <meta property="og:type" content="website"/>
             <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@_yihui"/>
             <meta name="twitter:title" content={title ? `${title} — henesys` : "henesys"} />
             <meta name="twitter:description" content={`henesys bookmark added by ${username}`} />
             <meta name="twitter:image" content={`https://henesys.online/api/og/?${searchParams.toString()}?`} />
