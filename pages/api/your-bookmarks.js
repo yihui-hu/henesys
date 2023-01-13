f42beed0efdd1e49198a25a4e9c1444a70c53af0
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-13 00:23:13 -0500

    more prod debugging

--- pages/api/your-bookmarks.js
+++ pages/api/your-bookmarks.js
@@ -15,24 +15,40 @@ const handler = async (req, res) => {
       return res.json({ status: "error", error: "Invalid token." });
     }
 
-    const lastTimestamp = req.query.lastTimestamp;
+    let lastTimestamp;
+    try {
+      lastTimestamp = req.query.lastTimestamp;
+    } catch (err) {
+      return res.json({
+        status: "error",
+        error: "Unable to retrieve last timestamp.",
+      });
+    }
+
     const limit = 36;
 
-    const bookmarks = await Bookmark.find({
-      username: decoded.username,
-      timestamp: { $lt: lastTimestamp },
-    })
-      .limit(limit)
-      .sort({ timestamp: -1 });
+    let bookmarks;
+    try {
+      bookmarks = await Bookmark.find({
+        username: decoded.username,
+        timestamp: { $lt: lastTimestamp },
+      })
+        .limit(limit)
+        .sort({ timestamp: -1 });
+    } catch (err) {
+      return res.json({
+        status: "error",
+        error: "Unable to retrieve bookmarks.",
+      });
+    }
 
-    const new_lastTimestamp = bookmarks?.at(-1).timestamp;
-
-    // console.log(bookmarks);
+    const new_lastTimestamp = bookmarks?.at(bookmarks.length - 1)?.timestamp;
 
     return res.json({
       status: "ok",
       bookmarks: bookmarks,
-      new_lastTimestamp: new_lastTimestamp,
+      new_lastTimestamp:
+        new_lastTimestamp == undefined ? 9999 : new_lastTimestamp,
     });
   } catch (err) {
     return res.json({ status: "error", error: err });
