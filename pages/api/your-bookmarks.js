a0277f511e2ad99cd0ac5eff537d188b1a99a80d
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-01-13 00:35:28 -0500

    better error checking

--- pages/api/your-bookmarks.js
+++ pages/api/your-bookmarks.js
@@ -4,50 +4,62 @@ const jwt = require("jsonwebtoken");
 
 const handler = async (req, res) => {
   // res.setHeader("Cache-Control", "s-maxage=1", "stale-while-revalidate");
-  let token;
-  let decoded;
   try {
-    token = req.headers["x-access-token"];
-    decoded = jwt.verify(token, process.env.FO_JWT_SECRET_KEY);
-  } catch (err) {
-    return res.json({ status: "error", error: "Invalid token." });
-  }
+    let token;
+    let decoded;
+    try {
+      token = req.headers["x-access-token"];
+      decoded = jwt.verify(token, process.env.FO_JWT_SECRET_KEY);
+    } catch (err) {
+      return res.json({ status: "error", error: "Invalid token." });
+    }
 
-  let lastTimestamp;
-  try {
-    lastTimestamp = req.query.lastTimestamp;
-  } catch (err) {
+    let lastTimestamp;
+    try {
+      lastTimestamp = req.query.lastTimestamp;
+    } catch (err) {
+      return res.json({
+        status: "error",
+        error: "Unable to retrieve last timestamp.",
+      });
+    }
+
+    const limit = 36;
+
+    let bookmarks;
+    try {
+      bookmarks = await Bookmark.find({
+        username: decoded.username,
+        timestamp: { $lt: lastTimestamp },
+      })
+        .limit(limit)
+        .sort({ timestamp: -1 });
+    } catch (err) {
+      return res.json({
+        status: "error",
+        error: "Unable to retrieve bookmarks.",
+      });
+    }
+
+    let new_lastTimestamp;
+    try {
+      new_lastTimestamp = bookmarks?.at(bookmarks.length - 1)?.timestamp;
+    } catch (err) {
+      return res.json({
+        status: "error",
+        error: "Unable to retrieve new last timestamp.",
+      });
+    }
+
     return res.json({
-      status: "error",
-      error: "Unable to retrieve last timestamp.",
+      status: "ok",
+      bookmarks: bookmarks,
+      new_lastTimestamp:
+        new_lastTimestamp == undefined ? 9999 : new_lastTimestamp,
     });
-  }
-
-  const limit = 36;
-
-  let bookmarks;
-  try {
-    bookmarks = await Bookmark.find({
-      username: decoded.username,
-      timestamp: { $lt: lastTimestamp },
-    })
-      .limit(limit)
-      .sort({ timestamp: -1 });
   } catch (err) {
-    return res.json({
-      status: "error",
-      error: "Unable to retrieve bookmarks.",
-    });
+    return res.json({ status: "error", error: err });
   }
-
-  const new_lastTimestamp = bookmarks?.at(bookmarks.length - 1)?.timestamp;
-
-  return res.json({
-    status: "ok",
-    bookmarks: bookmarks,
-    new_lastTimestamp:
-      new_lastTimestamp == undefined ? 9999 : new_lastTimestamp,
-  });
 };
 
 export default connectDB(handler);
